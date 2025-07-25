a8937ad046e46fee2087794f52b0cc8a84a48a6c
skrajput <skrajput42@gmail.com>
2025-07-25 09:59:05 +0530

    request body - pydantic

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from enum import Enum
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
@@ -16,3 +23,12 @@ async def home(model_name: ModelName):
 @app.get('/')
 async def home():
     return {"message":"Hi, this is first api in fastapi"}
+
+#Request Body
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
